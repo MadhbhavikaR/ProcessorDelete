5b73b1cfdb230beb3cb491ec6ad3948082babbd5
Prasad Madhbhavikar <pmadhbha@cisco.com>
2018-06-11 19:29:22 +0530

    Applied License and some pom modifications

--- src/main/java/online/madhbhavikar/processor/Application.java
+++ src/main/java/online/madhbhavikar/processor/Application.java
@@ -1,11 +1,25 @@
 /*
- * Copyright (c) 2018 MadhbhavikaR
+ * MIT License
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Copyright (c) 2018. MadhbhavikaR <connected.madhbhavikar@gmail.com>
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package online.madhbhavikar.processor;
@@ -16,28 +30,8 @@ import org.slf4j.LoggerFactory;
 
 public class Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-    private final String implementationVersion;
-    private final String implementationVendor;
-    private final String ImplementationTitle;
 
     private Application() {
-        //FIXME: getting null values : null vnull by null
-        Package aPackage = this.getClass().getPackage();
-        implementationVersion = aPackage.getImplementationVersion();
-        implementationVendor = aPackage.getImplementationVendor();
-        ImplementationTitle =  aPackage.getImplementationTitle();
-    }
-
-    public String getImplementationVersion() {
-        return implementationVersion;
-    }
-
-    public String getImplementationVendor() {
-        return implementationVendor;
-    }
-
-    public String getImplementationTitle() {
-        return ImplementationTitle;
     }
 
     private void run(String[] directories){
